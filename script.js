cee8b6958c23ffdf1f9b67c6ec5ce7cb96ce5a1e
Antoine FAVEREAU <antoine.favereau@viacesi.fr>
2023-07-01 12:54:24 +0200

    wip

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ canvas.height = 500;
 var ctx = canvas.getContext("2d");
 
 var square_width = 50;
-var game_speed = 10;
+var game_speed = 7;
 var ground_height = 100;
 
 
@@ -12,8 +12,8 @@ class Player {
     constructor() {
         this.width = square_width;
         this.jump = false;
-        this.jump_force = -10;
-        this.gravity = .4;
+        this.jump_force = -15;
+        this.gravity = 1;
         this.x = 250;
         this.y = canvas.height - ground_height;
         this.ay = 0;
@@ -57,22 +57,17 @@ class Player {
     }
 
     colliding() {
-        var level_array = level.level_array;
-
         var collide = false;
 
-        for (let i = 0; i < level_array.length; i++) {
-            for (let j = 0; j < level_array[i].length; j++) {
-                if (level_array[i][j]) {
-                    if (this.y + this.width >= canvas.height - ground_height - i * square_width && this.y <= canvas.height - ground_height - i * square_width + square_width && this.x + level_x + this.width >= j * square_width && this.x + level_x <= j * square_width + square_width) {
-                        collide = true;
-                        if (this.y + this.width - 10 >= canvas.height - ground_height - i * square_width) {
-                            clearInterval(game);
-                        }
-                    }
+        level.forEach(tile => {
+            if (this.y + this.width >= canvas.height - tile.y + square_width && this.y <= canvas.height - tile.y + square_width + tile.height && this.x + level_x + this.width >= tile.x && this.x + level_x <= tile.x + tile.width) {
+                collide = true;
+                if (this.y + this.width - 10 >= canvas.height - tile.y + square_width || tile.type == "spade") {
+                    stop = true;
+                    return true;
                 }
             }
-        }
+        })
 
         if (this.y >= canvas.height - ground_height) {
             return true;
@@ -82,34 +77,37 @@ class Player {
     }
 }
 
-class Level {
-    constructor(level) {
-        this.level_array = level;
-    }
-
-    draw() {
-        ctx.fillStyle = "#222";
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = "#fff";
-        for (let i = 0; i < this.level_array.length; i++) {
-            for (let j = 0; j < this.level_array[i].length; j++) {
-                if (this.level_array[i][j]) {
-                    ctx.beginPath();
-                    ctx.rect(j * square_width - level_x, canvas.height - ground_height - (i + 1) * square_width, square_width, square_width);
-                    ctx.fill();
-                    ctx.stroke();
-                    ctx.closePath();
-                }
-            }
-        }
-    }
-}
-
 class Tile {
-    constructor(type) {
-        switch (type) {
-            case "block":
+    constructor(TYPE, X, Y) {
+        this.type = TYPE;
 
+        switch (this.type) {
+            case "block":
+                this.x = X;
+                this.y = Y + ground_height;
+                this.width = square_width;
+                this.height = square_width;
+                break;
+
+            case "slab":
+                this.x = X;
+                this.y = Y + ground_height;
+                this.width = square_width;
+                this.height = square_width / 2;
+                break;
+
+            case "spade":
+                this.x = X;
+                this.y = Y + ground_height;
+                this.width = square_width;
+                this.height = square_width;
+                break;
+
+            case "doublespade":
+                this.x = X;
+                this.y = Y + ground_height;
+                this.width = square_width;
+                this.height = square_width;
                 break;
 
             default:
@@ -118,37 +116,78 @@ class Tile {
     }
 
     draw() {
+        ctx.fillStyle = "#222";
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "#fff";
+        ctx.beginPath();
+        switch (this.type) {
+            case "spade":
+                ctx.moveTo(this.x - level_x + this.width / 2, canvas.height - this.y);
+                ctx.lineTo(this.x - level_x + this.width, canvas.height - this.y + this.height);
+                ctx.lineTo(this.x - level_x, canvas.height - this.y + this.height);
+                ctx.lineTo(this.x - level_x + this.width / 2, canvas.height - this.y);
+                break;
 
+            default:
+                ctx.rect(this.x - level_x, canvas.height - this.y, this.width, this.height);
+                break;
+        }
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
     }
 }
 
-var level_1 = [
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, new Tile("block"), new Tile("block"), new Tile("block"), new Tile("block"), false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, new Tile("block"), new Tile("block"), false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, new Tile("block"), new Tile("block"), new Tile("block"), false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+const level_1 = [
+    new Tile("spade", square_width * 18, square_width * 1), new Tile("doublespade", square_width * 30, square_width * 1), 
 ]
-var player = new Player();
-var level = new Level(level_1);
-var level_x = 0
 
-var game = setInterval(() => {
-    level_x += 2;
-    player.move();
-    draw();
-    level.draw();
-    player.draw();
-}, 10)
+var player;
+var level;
+var level_x
+var game;
+var attempt = 0;
+var stop;
+var previousTime;
+var frameTime;
 
-function draw() {
+newGame();
+
+
+function newGame() {
+    player = new Player();
+    level = level_1;
+    level_x = 0;
+    attempt++;
+    stop = false;
+    previousTime = 0;
+    frameTime = 1000 / 60;
+
+    requestAnimationFrame(update);
+}
+
+function update() {
+    var currentTime = (new Date()).getTime();
+    var elapsed = currentTime - previousTime;
+
+    if (elapsed > frameTime) {
+        level_x += game_speed;
+        player.move();
+        draw(attempt);
+        player.draw();
+
+        previousTime = currentTime - (elapsed % frameTime);
+    }
+
+    if (stop) {
+        newGame();
+    } else {
+        requestAnimationFrame(update);
+    }
+}
+
+
+function draw(attempt) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#111";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -159,4 +198,10 @@ function draw() {
     ctx.ellipse(canvas.width / 2, canvas.height - ground_height + 1, 2, 300, Math.PI / 2, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
+    ctx.font = "52px Arial";
+    // ctx.textAlign = "center";
+    ctx.fillText("Essai " + attempt, canvas.width / 2 - level_x / 2, 150);
+    level.forEach(tile => {
+        tile.draw();
+    })
 }
